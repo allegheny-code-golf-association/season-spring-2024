3da1975a697c376a7b73f81ea3381712300bb3fa
dluman <dluman@allegheny.edu>
2024-04-29 07:15:40 -0400

    Updating round-15

---
+++ 15/src/test/java/com/golf/two/BaseTests.java
@@ -0,0 +1,79 @@
+package com.golf.six;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.PrintStream;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.interpreter.lox.Main;
+
+class BaseTests {
+
+  /*
+    Word to the accepted answer at SO, they're a real one.
+    https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+  */
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  @BeforeAll
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  static Stream<Arguments> testProgramOutput() throws Exception {
+    URL resource = BaseTests.class.getClassLoader().getResource("main.lox");
+    File file = Paths.get(resource.toURI()).toFile();
+    String absPath = file.getAbsolutePath();
+    return Stream.of(
+      Arguments.of((Object) new String[]{absPath})
+    );
+  }
+
+  @MethodSource
+  @ParameterizedTest
+  void testProgramOutput(String[] args) throws Exception {
+    Main.main(args);
+    assertEquals(
+        "**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n" +
+"**********\n",
+        outContent.toString()
+    );
+  }
+
+  @AfterAll
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
+
+}
